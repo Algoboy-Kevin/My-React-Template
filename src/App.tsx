c5e5d40ed259e52f3efaee8e6d00e1ba61047495
Kevin Gamaliel <gamagambogam@gmail.com>
2022-01-20 16:02:42 +0700

    second commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import {themeChange} from 'theme-change'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  
+  useEffect(() => themeChange(false), [])
+  
   return (
     <div className="hero min-h-screen bg-base-200">
       <header className="text-center hero-content ">
@@ -11,9 +13,16 @@ function App() {
             Hello World
           </h1>
           <p className="mb-5">
-            Welcome to AlgoBoy's basic setup. If this page is not displaying properly, please check your settings. Happy hacking!
+            Welcome to AlgoBoy-Kevin's basic setup. If this page is not displaying properly, please check your settings. Happy hacking!
           </p>
-          <button className='btn btn-primary'>Get Started</button>
+          <select data-choose-theme className=' text-black'>
+            <option value="">Change Theme</option>
+            <option value="light">Light</option>
+            <option value="dark">Dark</option>
+            <option value="cupcake">Cupcake</option>
+            <option value="cyberpunk">Cyberpunk</option>
+            <option value="synthwave">Synthwave</option>
+          </select>
         </div>
     
       </header>
